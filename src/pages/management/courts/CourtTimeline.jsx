c824a66ffbe0892c7dfb20abf4b8b3a016fb74ff
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2022-12-05 20:49:45 +0100

    added scheduler component

--- src/pages/management/courts/CourtTimeline.jsx
+++ src/pages/management/courts/CourtTimeline.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { Input, SubmitButton } from "../../../components/form";
+import { Scheduler } from "@aldabil/react-scheduler";
 
 export default function () {
   const loading = useSelector((state) => state.management.loading);
@@ -13,8 +14,24 @@ export default function () {
     <div className="center" style={{ marginTop: 20 }}>
       <div className="card main-container card-max-height">
         <h4 className="card-header">Timeline</h4>
-        <div>
-          ...
+        <div className="card-body">
+          <Scheduler
+            view="week"
+            events={[
+              {
+                event_id: 1,
+                title: "Event 1",
+                start: new Date("2022/12/2 09:30"),
+                end: new Date("2022/12/2 15:30"),
+              },
+              {
+                event_id: 2,
+                title: "Event 2",
+                start: new Date("2022/12/4 10:00"),
+                end: new Date("2022/12/4 13:00"),
+              },
+            ]}
+          />
         </div>
       </div>
     </div>
