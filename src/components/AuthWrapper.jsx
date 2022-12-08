ca7ca74577605b0b52df5c7615e76880661697fa
Brina Jehart <brina.jehart@gmail.com>
2022-12-08 13:19:22 +0100

    added favicon and minor redesign

--- src/components/AuthWrapper.jsx
+++ src/components/AuthWrapper.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import "../assets/authform.css";
-import Logo from '../assets/LogoVertical.svg';
+import Logo from "../assets/LogoVertical.svg";
 
 export default function ({ children }) {
   return (
     <div className="auth-wrapper">
       <div className="center banner">
-        <img
-          src={Logo}
-          style={{ width: 250 }}
-        />
-        <figure className="text-center">
+        <img src={Logo} style={{ width: 250 }} />
+        <figure className="text-center" style={{ width: "66%" }}>
           <blockquote className="blockquote">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
